27e5cbbb892bbb39550dff5a2a8f665c66b9f4a2
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-01-10 10:39:39 +0000

    WIP for Workers

--- worker-db-ddl.sql
+++ worker-db-ddl.sql
@@ -1,10 +1,8 @@
 -- DDL for Workers
 
 -- DROP/CLEAN SCHEMA
-DROP TYPE IF EXISTS cqc."WorkerContract";
 DROP TABLE IF EXISTS cqc."Worker";
-
-
+DROP TYPE IF EXISTS cqc."WorkerContract";
 
 -- CREATE/RE-CREATE SCHEMA
 CREATE TYPE cqc."WorkerContract" AS ENUM (
@@ -15,21 +13,22 @@ CREATE TYPE cqc."WorkerContract" AS ENUM (
 	'Other'
 );
 
+
 CREATE TABLE IF NOT EXISTS cqc."Worker" (
 	"ID" serial NOT NULL PRIMARY KEY,
-	"WorkerID" uuid NOT NULL,
-	"EstablishmentFk" integer NOT NULL,
+	"WorkerUID" uuid NOT NULL,
+	"EstablishmentFK" integer NOT NULL,
 	"NameOrID" varchar(50),
 	"Contract" cqc."WorkerContract",
 	"MainJobFK" INTEGER,
 	created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
 	updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),	-- note, on creation of record, updated and created are equal
     CONSTRAINT "Worker_ID_unq" UNIQUE ("ID"),
-    CONSTRAINT "Worker_Establishment_fk" FOREIGN KEY ("EstablishmentFk") REFERENCES cqc."Establishment" ("EstablishmentID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT "Worker_Establishment_fk" FOREIGN KEY ("EstablishmentFK") REFERENCES cqc."Establishment" ("EstablishmentID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
 	CONSTRAINT "Worker_Job_mainjob_fk" FOREIGN KEY ("MainJobFK") REFERENCES cqc."Job" ("JobID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT "Worker_NameID_unq" UNIQUE ("NameOrID", "EstablishmentFk")
+	CONSTRAINT "Worker_NameID_unq" UNIQUE ("NameOrID", "EstablishmentFK"),
+	CONSTRAINT "Worker_WorkerUID_unq" UNIQUE ("WorkerUID", "EstablishmentFK")
 );
-
 
 -- REFERENCE DATA
 
