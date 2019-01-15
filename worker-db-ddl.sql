37ec21072b9dafbf71af7be2c10690ada456ea26
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2019-01-15 14:22:44 +0000

    Removing the artificial composite unique constraint on name/id with establishment id

--- worker-db-ddl.sql
+++ worker-db-ddl.sql
@@ -34,9 +34,10 @@ CREATE TABLE IF NOT EXISTS cqc."Worker" (
 	updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),	-- note, on creation of record, updated and created are equal
     CONSTRAINT "Worker_Establishment_fk" FOREIGN KEY ("EstablishmentFK") REFERENCES cqc."Establishment" ("EstablishmentID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
 	CONSTRAINT "Worker_Job_mainjob_fk" FOREIGN KEY ("MainJobFK") REFERENCES cqc."Job" ("JobID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT "Worker_NameID_unq" UNIQUE ("NameOrID", "EstablishmentFK"),
 	CONSTRAINT "Worker_WorkerUID_unq" UNIQUE ("WorkerUID", "EstablishmentFK")
 );
 
 CREATE UNIQUE INDEX "Worker_WorkerUID" on cqc."Worker" ("WorkerUID");
 CREATE INDEX "Worker_EstablishmentFK" on cqc."Worker" ("EstablishmentFK");
+
+
