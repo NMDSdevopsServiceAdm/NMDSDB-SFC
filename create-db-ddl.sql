69d89555460359d9a8ddf44aaa1c3a356552f62b
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-02-28 10:28:17 +0000

    When updating UUID, need sto be a V4 UUID

--- create-db-ddl.sql
+++ create-db-ddl.sql
@@ -1215,11 +1215,15 @@ ALTER TABLE cqc."User" ADD COLUMN "UserUID" UUID NULL;
 UPDATE
 	cqc."User" 
 SET
-	"UserUID" = "USER_UUID"."UID"
+	"UserUID" = "USER_UUID"."UIDv4"
 FROM (
-	SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) "UID", "User"."RegistrationID" "RegID"
-	from cqc."User", cqc."Login"
-	where "User"."RegistrationID" = "Login"."RegistrationID"
+	SELECT CAST(substr(CAST(myuuids."UID" AS TEXT), 0, 15) || '4' || substr(CAST(myuuids."UID" AS TEXT), 16, 3) || '-89' || substr(CAST(myuuids."UID" AS TEXT), 22, 36) AS UUID) "UIDv4", "RegID"
+    FROM (
+        SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) "UID",
+                "User"."RegistrationID" "RegID"
+        FROM cqc."User", cqc."Login"
+        WHERE "User"."RegistrationID" = "Login"."RegistrationID"
+	) AS MyUUIDs
 ) AS "USER_UUID"
 WHERE "USER_UUID"."RegID" = "User"."RegistrationID";
 
