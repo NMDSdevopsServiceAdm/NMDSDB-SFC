9f156f088c04d52c3dfd3ae8171933cae676d45f
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-02-28 09:45:01 +0000

    Dropping previous patch SQL, and adding the User::uid column, with new patch SQL

--- create-db-ddl.sql
+++ create-db-ddl.sql
@@ -333,6 +333,7 @@ ALTER TABLE cqc."ServicesCapacity" OWNER TO sfcadmin;
 
 CREATE TABLE IF NOT EXISTS cqc."User" (
     "RegistrationID" integer NOT NULL,
+    "UserUID" UUID NOT NULL,
     "EstablishmentID" integer NOT NULL,
     "FullNameValue" character varying(120) NOT NULL,
     "FullNameSavedAt" TIMESTAMP NULL,
@@ -1206,135 +1207,20 @@ CREATE TABLE IF NOT EXISTS cqc."UserAudit" (
 );
 CREATE INDEX "UserAudit_UserFK" on cqc."UserAudit" ("UserFK");
 
--- DB Patch Schema for https://trello.com/c/pByUKSW3
-DROP TABLE IF EXISTS  cqc."WorkerAudit";
-DROP TYPE IF EXISTS cqc."WorkerAuditChangeType";
-CREATE TYPE cqc."WorkerAuditChangeType" AS ENUM (
-	'created',
-	'updated',
-	'saved',
-	'changed'
-);
-CREATE TABLE IF NOT EXISTS cqc."WorkerAudit" (
-	"ID" SERIAL NOT NULL PRIMARY KEY,
-	"WorkerFK" INTEGER NOT NULL,
-	"Username" VARCHAR(120) NOT NULL,
-	"When" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-	"EventType" cqc."WorkerAuditChangeType" NOT NULL,
-	"PropertyName" VARCHAR(100) NULL,
-	"ChangeEvents" JSONB NULL,
-	CONSTRAINT "WorkerAudit_Worker_fk" FOREIGN KEY ("WorkerFK") REFERENCES cqc."Worker" ("ID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-CREATE INDEX "WorkerAudit_WorkerFK" on cqc."WorkerAudit" ("WorkerFK");
+-- DB Patch Schema - https://trello.com/c/pByUKSW3 - add UUID to User
+ALTER TABLE cqc."User" ADD COLUMN "UserUID" UUID NULL;
 
-ALTER TABLE cqc."Login" ADD COLUMN "PasswdLastChanged" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW();
-
-CREATE SEQUENCE IF NOT EXISTS cqc."PasswdResetTracking_seq"
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-    
-CREATE TABLE IF NOT EXISTS cqc."PasswdResetTracking" (
-    "ID" INTEGER NOT NULL PRIMARY KEY,
-	"UserFK" INTEGER NOT NULL,
-    "Created" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-    "Expires" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW() + INTERVAL '24 hour',
-    "ResetUuid"  UUID NOT NULL,
-    "Completed" TIMESTAMP NULL,
-	CONSTRAINT "PasswdResetTracking_User_fk" FOREIGN KEY ("UserFK") REFERENCES cqc."User" ("RegistrationID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-ALTER TABLE cqc."PasswdResetTracking" ALTER COLUMN "ID" SET DEFAULT nextval('cqc."PasswdResetTracking_seq"');
-
-
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestion" character varying(255) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionAnswer" character varying(255) NULL;
--- migrate security question/answer from Login to User
+-- unfortunately, without the postgres extension "uuid-ossp", need an alternative method to
+--  update existing User records with UUID
 UPDATE
-	cqc."User"
+	cqc."User" 
 SET
-	"SecurityQuestion" = login."SecurityQuestion",
-    "SecurityQuestionAnswer" = login."SecurityQuestionAnswer"	
-FROM
-	cqc."Login" as login
-WHERE
-	login."RegistrationID" = "User"."RegistrationID";
--- note - the security question/answer are not mandatory (later task to add users means they must be left null) so leave them nullable
--- and now drop the columns from Login
-ALTER TABLE cqc."Login" DROP COLUMN "SecurityQuestion";
-ALTER TABLE cqc."Login" DROP COLUMN "SecurityQuestionAnswer";
+	"UserUID" = "USER_UUID"."UID"
+FROM (
+	SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) "UID", "User"."RegistrationID" "RegID"
+	from cqc."User", cqc."Login"
+	where "User"."RegistrationID" = "Login"."RegistrationID"
+) AS "USER_UUID"
+WHERE "USER_UUID"."RegID" = "User"."RegistrationID";
 
--- and now rename the User columns ready for Extended Change Properties
-ALTER TABLE cqc."User" RENAME "FullName" TO "FullNameValue";
-ALTER TABLE cqc."User" RENAME "JobTitle" TO "JobTitleValue";
-ALTER TABLE cqc."User" RENAME "Email" TO "EmailValue";
-ALTER TABLE cqc."User" RENAME "Phone" TO "PhoneValue";
-ALTER TABLE cqc."User" RENAME "SecurityQuestion" TO "SecurityQuestionValue";
-ALTER TABLE cqc."User" RENAME "SecurityQuestionAnswer" TO "SecurityQuestionAnswerValue";
-
--- and now add the additional the User columns ready for Extended Change Properties
-ALTER TABLE cqc."User" ADD COLUMN "FullNameSavedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "FullNameChangedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "FullNameSavedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "FullNameChangedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "JobTitleSavedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "JobTitleChangedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "JobTitleSavedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "JobTitleChangedBy" VARCHAR(120) ==NULL;
-ALTER TABLE cqc."User" ADD COLUMN "EmailSavedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "EmailChangedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "EmailSavedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "EmailChangedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "PhoneSavedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "PhoneChangedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "PhoneSavedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "PhoneChangedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionSavedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionChangedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionSavedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionChangedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionAnswerSavedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionAnswerChangedAt" TIMESTAMP NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionAnswerSavedBy" VARCHAR(120) NULL;
-ALTER TABLE cqc."User" ADD COLUMN "SecurityQuestionAnswerChangedBy" VARCHAR(120) NULL;
-
-
--- add the created/updated/updatedBy columns
-ALTER TABLE cqc."User" ADD COLUMN created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW();
-ALTER TABLE cqc."User" ADD COLUMN updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW();
-ALTER TABLE cqc."User" ADD COLUMN updatedby VARCHAR(120) NULL;
-UPDATE cqc."User" set updatedby='admin';                            -- cannot be null, so setting a default value on apply patch
-ALTER TABLE cqc."User" ALTER COLUMN updatedby SET NOT NULL;
-
--- and drop the now unused "DateCreated" column
-ALTER TABLE cqc."User" DROP COLUMN "DateCreated";
-
-CREATE TYPE cqc."UserAuditChangeType" AS ENUM (
-    'created',
-    'updated',
-    'saved',
-    'changed',
-    'passwdReset',
-    'loginSuccess',
-    'loginFailed',
-    'loginWhileLocked'
-);
-CREATE TABLE IF NOT EXISTS cqc."UserAudit" (
-	"ID" SERIAL NOT NULL PRIMARY KEY,
-	"UserFK" INTEGER NOT NULL,
-	"Username" VARCHAR(120) NOT NULL,
-	"When" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-	"EventType" cqc."UserAuditChangeType" NOT NULL,
-	"PropertyName" VARCHAR(100) NULL,
-	"ChangeEvents" JSONB NULL,
-	CONSTRAINT "WorkerAudit_User_fk" FOREIGN KEY ("UserFK") REFERENCES cqc."User" ("RegistrationID") MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-CREATE INDEX "UserAudit_UserFK" on cqc."UserAudit" ("UserFK");
-
--- https://trello.com/c/1f4RSnlu defect fix
-DROP INDEX IF EXISTS cqc."Establishment_unique_registration";
-DROP INDEX IF EXISTS cqc."Establishment_unique_registration_with_locationid";
-CREATE UNIQUE INDEX IF NOT EXISTS "Establishment_unique_registration" ON cqc."Establishment" ("Name", "PostCode", "LocationID");
-CREATE UNIQUE INDEX IF NOT EXISTS "Establishment_unique_registration_with_locationid" ON cqc."Establishment" ("Name", "PostCode") WHERE "LocationID" IS NULL;
+ALTER TABLE cqc."User" ALTER COLUMN "UserUID" SET NOT NULL;
