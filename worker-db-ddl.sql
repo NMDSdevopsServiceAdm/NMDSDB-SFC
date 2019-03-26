75abc9b91a52ec18280288fae9ce56b5237fee6a
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-03-26 06:17:01 +0000

    Now the worker patch has been applied to dev and test, this is now ready to go to UAT. Removing the patch sql, because UAT has no Worker to patch.

--- worker-db-ddl.sql
+++ worker-db-ddl.sql
@@ -889,25 +889,3 @@ CREATE TABLE IF NOT EXISTS cqc."WorkerJobs" (
 );
 CREATE INDEX "WorkerJobs_WorkerFK" on cqc."WorkerJobs" ("WorkerFK");
 CREATE INDEX "WorkerJobs_JobFK" on cqc."WorkerJobs" ("JobFK");
-
--- Worker patch - https://trello.com/c/6bctXaoy
--- Worker's Leave Reasons
-CREATE TABLE IF NOT EXISTS cqc."WorkerLeaveReasons" (
-	"ID" INTEGER NOT NULL PRIMARY KEY,
-	"Seq" INTEGER NOT NULL, 	-- this is the order in which the Ethinicity will appear without impacting on primary key (existing foreign keys)
-	"Reason" TEXT NOT NULL
-);
-INSERT INTO cqc."WorkerLeaveReasons" ("ID", "Seq", "Reason") VALUES 
-	(1, 1, 'They moved to another adult social care employer'),
-	(2, 2, 'They moved to a role in the health sector'),
-	(3, 3, 'They moved to a different sector (e.g. retail)'),
-	(4, 4, 'They moved to another role in this organisation'),
-	(5, 5, 'The worker chose to leave (destination unknown)'),
-	(6, 6, 'The worker retired'),
-	(7, 7, 'Employer terminated their employment'),
-	(8, 8, 'Other'),
-	(9, 9, 'Not known');
-
-ALTER TABLE cqc."Worker" ADD COLUMN "LeaveReasonFK" INTEGER NULL;
-ALTER TABLE cqc."Worker" ADD COLUMN "LeaveReasonOther" TEXT NULL;
-ALTER TABLE cqc."Worker" ADD CONSTRAINT "Worker_leave_reason_fk" FOREIGN KEY ("LeaveReasonFK") REFERENCES cqc."WorkerLeaveReasons" ("ID");
