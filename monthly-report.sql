66f8c5219d1898392d70f847743e1f22f1261cb3
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-04-04 11:21:11 +0100

    Reporting Table

---
+++ monthly-report.sql
@@ -0,0 +1,48 @@
+
+--Monthly Report
+
+select "EstablishmentName",
+"EstablishmentID",
+"TotalWorkers" ,
+"EmailAddress",
+case when "Vacancies" is null then 0 else "Vacancies" end,
+case when "Starters" is null then 0 else "Starters" end,
+case when "Leavers" is null then 0 else "Leavers" end
+from (
+select "Establishment"."Name" "EstablishmentName",
+"Establishment"."EstablishmentID" "EstablishmentID",
+"Establishment"."NumberOfStaff" "TotalWorkers",
+"User"."Email" "EmailAddress",
+sum(case when "JobType" = 'Vacancies' then "Total" end) "Vacancies",
+sum(case when "JobType" = 'Starters' then "Total" end) "Starters",
+sum(case when "JobType" = 'Leavers' then "Total" end) "Leavers"
+from cqc."EstablishmentJobs", cqc."Establishment",cqc."User"
+where "Establishment"."EstablishmentID" = "EstablishmentJobs"."EstablishmentID" and "Establishment"."EstablishmentID"="User"."EstablishmentID"
+group by "Establishment"."Name", "Establishment"."EstablishmentID","Establishment"."NumberOfStaff","User"."Email"
+) as MyWorker
+order by "EstablishmentName" asc;
+*/
+create temp view  v1 as
+select "EstablishmentName",
+    "EstablishmentID",
+    "TotalWorkers" ,
+    "EmailAddress",
+    case when "Vacancies" is null then 0 else "Vacancies" end,
+    case when "Starters" is null then 0 else "Starters" end,
+    case when "Leavers" is null then 0 else "Leavers" end
+from (
+    select
+        "Establishment"."Name" "EstablishmentName",
+        "Establishment"."EstablishmentID" "EstablishmentID",
+        "Establishment"."NumberOfStaff" "TotalWorkers",
+        "User"."EmailValue" "EmailAddress",
+        sum(case when "JobType" = 'Vacancies' then "Total" end) "Vacancies",
+        sum(case when "JobType" = 'Starters' then "Total" end) "Starters",
+        sum(case when "JobType" = 'Leavers' then "Total" end) "Leavers"
+    from cqc."EstablishmentJobs", cqc."Establishment",cqc."User"
+    where "Establishment"."EstablishmentID" = "EstablishmentJobs"."EstablishmentID" and "Establishment"."EstablishmentID"="User"."EstablishmentID"
+    group by "Establishment"."Name", "Establishment"."EstablishmentID","Establishment"."NumberOfStaff","User"."EmailValue"
+) as MyWorker
+order by "EstablishmentName" asc;
+
+
