09e68abff3fb54b730df01de09039353737366e9
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-04-18 17:01:40 +0100

    Adding Migration of Establishments too

--- UAT/migration_patch.sql
+++ UAT/migration_patch.sql
@@ -130,3 +130,135 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION cqc.MigrateEstablishments()
+	RETURNS void AS $$
+DECLARE
+  AllEstablishments REFCURSOR;
+  CurrentEstablishment RECORD;
+  NotMapped VARCHAR(10);
+  MappedEmpty VARCHAR(10);
+  MigrationUser VARCHAR(10);
+  ThisEstablishmentID INTEGER;
+  NewEstablishmentUID UUID;
+  MigrationTimestamp timestamp without time zone;
+  FullAddress TEXT;
+  NewIsRegulated BOOLEAN;
+  NewEmployerType VARCHAR(40);
+  NewIsCqcRegistered BOOLEAN;
+BEGIN
+	NotMapped := 'Not Mapped';
+	MappedEmpty := 'Was empty';
+	MigrationUser := 'migration';
+	MigrationTimestamp := clock_timestamp();
+	
+  -- first clean up any already migrated data
+  -- TODO - tidy up EstablishmentAudit records
+  DELETE FROM cqc."Establishment" where "TribalID" is not null;
+
+  OPEN AllEstablishments FOR 	select
+			e.id,
+			e.name,
+			e.address1,
+			e.address2,
+			e.address3,
+			e.town,
+			p.locationid,
+			e.postcode,
+			e.type as employertypeid,
+			p.totalstaff as numberofstaff,
+			e.nmdsid,
+			e.createddate
+		from establishment e
+			  inner join provision p on p.establishment_id = e.id
+--			  inner join establishment_user eu on eu.establishment_id = e.id
+--				inner join users on eu.user_id = users.id
+--		where
+--		  users.mustchangepassword = 0
+		 order by e.id asc;
+
+  LOOP
+  	BEGIN
+		FETCH AllEstablishments INTO CurrentEstablishment;
+		EXIT WHEN NOT FOUND;
+
+		FullAddress = CurrentEstablishment.address1 || ', ' || CurrentEstablishment.address2 || ', ' || CurrentEstablishment.address3 || ', ' || CurrentEstablishment.town;
+
+		-- target Establishment needs a UID; unlike User, there is no UID in tribal dataset
+		SELECT CAST(substr(CAST(v1uuid."UID" AS TEXT), 0, 15) || '4' || substr(CAST(v1uuid."UID" AS TEXT), 16, 3) || '-89' || substr(CAST(v1uuid."UID" AS TEXT), 22, 36) AS UUID)
+			FROM (
+				SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) "UID"
+			) v1uuid
+		INTO NewEstablishmentUID;
+
+		CASE CurrentEstablishment.locationid
+			WHEN NULL THEN
+				NewIsRegulated = false;
+			ELSE
+				NewIsRegulated = true;
+		END CASE;
+
+		CASE CurrentEstablishment.employertypeid
+			WHEN 130 THEN
+				NewEmployerType = 'Local Authority (adult services)';
+			WHEN 131 THEN
+				NewEmployerType = 'Local Authority (generic/other)';
+			WHEN 132 THEN
+				NewEmployerType = 'Local Authority (generic/other)';
+			WHEN 133 THEN
+				NewEmployerType = 'Local Authority (generic/other)';
+			WHEN 134 THEN
+				NewEmployerType = 'Other';
+			WHEN 135 THEN
+				NewEmployerType = 'Private Sector';
+			WHEN 136 THEN
+				NewEmployerType = 'Voluntary / Charity';
+			WHEN 137 THEN
+				NewEmployerType = 'Other';
+			WHEN 138 THEN
+				NewEmployerType = 'Private Sector';
+			ELSE
+				NewEmployerType = 'Other';
+		END CASE;
+		
+		SELECT nextval('cqc."Establishment_EstablishmentID_seq"') INTO ThisEstablishmentID;
+		INSERT INTO cqc."Establishment" (
+			"EstablishmentID",
+			"TribalID",
+			"EstablishmentUID",
+			"NameValue",
+			"Address",
+			"LocationID",
+			"PostCode",
+			"IsRegulated",
+			"NmdsID",
+			"EmployerTypeValue",
+			"NumberOfStaffValue",
+			"created",
+			"updated",
+			"updatedby"
+		) VALUES (
+			ThisEstablishmentID,
+			CurrentEstablishment.id,
+			NewEstablishmentUID,
+			CurrentEstablishment.name,
+			FullAddress,
+			CurrentEstablishment.locationid,
+			CurrentEstablishment.postcode,
+			NewIsRegulated,
+			CurrentEstablishment.nmdsid,
+			NewEmployerType::cqc.est_employertype_enum,
+			CurrentEstablishment.numberofstaff,
+			CurrentEstablishment.createddate,
+			MigrationTimestamp,
+			MigrationUser
+		  );
+		  
+  	EXCEPTION WHEN OTHERS THEN
+		RAISE NOTICE 'Skipping establishment with id: %', CurrentEstablishment.id;
+	END;
+  END LOOP;
+
+END;
+$$ LANGUAGE plpgsql;
