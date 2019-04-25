280870f3ac52a277ae5c7cc759207b615acd8fa1
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-04-25 19:21:16 +0100

    Live data correction

---
+++ Live_data_update.sql
@@ -0,0 +1,10 @@
+https://trello.com/c/CKuDXAbq
+update cqc."Establishment" set "MainServiceFKValue"=20 where "EstablishmentID"=340;
+
+
+
+
+https://trello.com/c/oeYhp3Ui
+
+update cqc."Establishment" set "IsRegulated"='t' where "EstablishmentID"=340;
+
