694053e982fac57fc719a22df527961237dc0200
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-04-26 09:53:39 +0100

    Corrections for EstablishmentID and boolean value

--- Live_data_update.sql
+++ Live_data_update.sql
@@ -1,10 +1,10 @@
 https://trello.com/c/CKuDXAbq
-update cqc."Establishment" set "MainServiceFKValue"=20 where "EstablishmentID"=340;
+update cqc."Establishment" set "MainServiceFKValue"=20 where "EstablishmentID"=340 and "LocationID"='1-5254278626';
 
 
 
 
 https://trello.com/c/oeYhp3Ui
 
-update cqc."Establishment" set "IsRegulated"='t' where "EstablishmentID"=340;
+update cqc."Establishment" set "IsRegulated"=True where "EstablishmentID"=337;
 
