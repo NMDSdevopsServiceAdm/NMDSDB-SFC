b37ed938d19803f96323b2544b3ae914ae3307c3
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-05-08 12:15:30 +0100

    Added verifcation steps as well

--- Live_data_update.sql
+++ Live_data_update.sql
@@ -1,6 +1,11 @@
 https://trello.com/c/CKuDXAbq
 update cqc."Establishment" set "MainServiceFKValue"=20 where "EstablishmentID"=340 and "LocationID"='1-5254278626';
 
+--Verify the change reflected in table
+
+\x 
+
+select * from cqc."Establishment" where  "EstablishmentID"=340 and "LocationID"='1-5254278626';
 
 
 
@@ -8,5 +13,9 @@ https://trello.com/c/oeYhp3Ui
 
 
 update cqc."Establishment" set "IsRegulated"=True,"LocationID"='1-5400068426' where "EstablishmentID"=337 ;
+
+--Verify the change reflected in table
+
+select * from cqc."Establishment" where "EstablishmentID"=337 ;
 
 
