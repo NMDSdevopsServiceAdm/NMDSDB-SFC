d5264b05862c418713268405429c9983991e0ed0
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-05-08 14:33:37 +0100

    Adding two additional capacities identified as missing during migration acceptance.

--- UAT/V1.9-patch.sql
+++ UAT/V1.9-patch.sql
@@ -6,3 +6,8 @@ INSERT INTO cqc."Qualifications" ("ID", "Seq", "Group", "Title", "Code", "From",
 UPDATE cqc."Qualifications"
 SET "Level" = NULL
 WHERE "ID" in (25, 26, 89, 113, 114, 115, 116);
+
+
+-- Additional service capacities -  https://trello.com/c/O1RxWYlU
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question") values (17, 21, 1, 'Number of people receiving care on the completion date');
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question") values (18, 23, 1, 'Number of people using the service on the completion date');
