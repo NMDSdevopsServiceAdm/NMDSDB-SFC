49d26dbd7a5f323783c53f6edc6603b964460b7e
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-05-14 14:07:03 +0100

    Adding to Establishment "Owner" and "OwnerDataAccess" attributes.

---
+++ UAT/V1.10_patch.sql
@@ -0,0 +1,15 @@
+-- https://trello.com/c/ikScd2O3 - parents & subs - view my workplaces
+
+
+CREATE TYPE cqc.establisgment_owner AS ENUM (
+    'Workplace',
+    'Parent'
+);
+
+CREATE TYPE cqc.establisgment_owner_access_permission AS ENUM (
+    'Workplace',
+    'Worker'
+);
+
+ALTER TABLE cqc."Establishment" ADD COLUMN "Owner" cqc.establisgment_owner NOT NULL DEFAULT 'Workplace';
+ALTER TABLE cqc."Establishment" ADD COLUMN "OwnerDataAccess" cqc.establisgment_owner_access_permission NULL;
