3d7300242a6b34013712616ea60f60f4fadb1d0d
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-05-16 14:22:43 +0100

    Records deletion for 2 establishment

--- Live_data_update.sql
+++ Live_data_update.sql
@@ -1,21 +1,80 @@
-https://trello.com/c/CKuDXAbq
-update cqc."Establishment" set "MainServiceFKValue"=20 where "EstablishmentID"=340 and "LocationID"='1-5254278626';
+-- https://trello.com/c/Ypwr3hZ4/55-remove-parent-cohort-2-users-from-asc-wds
 
---Verify the change reflected in table
+--Deletion data view before we delete the Establishment and related records
 
-\x 
-
-select * from cqc."Establishment" where  "EstablishmentID"=340 and "LocationID"='1-5254278626';
+-- Selection based on Tribal ID [196840,170258]
 
 
+select "TribalID"   from cqc."Establishment" where "NmdsID" IN ('E302906','G260465');
 
-https://trello.com/c/oeYhp3Ui
+SELECT * FROM cqc."Worker" where "TribalID" in (select "TribalID" from cqc."Establishment" where "NmdsID" IN ('E302906','G260465')) ;
+
+select  *    from cqc."WorkerQualification" where "TribalID" in (170258, 196840);
+  select  *    from cqc."WorkerTraining" where "TribalID"  in (170258, 196840);
+  select  *    from cqc."WorkerAudit" where "WorkerFK" in (select distinct "ID" from cqc."Worker" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."WorkerJobs" where "WorkerFK" in (select distinct "ID" from cqc."Worker" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."Worker" where "TribalID"  in (170258, 196840);
+  select  *    from cqc."EstablishmentCapacity" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentJobs" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentServices" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentLocalAuthority" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentServiceUsers" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+
+  select  *    from cqc."Login" where "RegistrationID" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."UserAudit" where "UserFK" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."PasswdResetTracking" where "UserFK" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."User" where "TribalID"  in (170258, 196840);
+  select  *    from cqc."EstablishmentAudit" where "EstablishmentFK" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."Establishment" where "TribalID"  in (170258, 196840);
+
+--- Record Deletion 
+
+-- Deletion based on Tribal ID [196840,170258]
+
+delete from cqc."WorkerQualifications" where "TribalID" in (170258, 196840);
+  delete from cqc."WorkerTraining" where "TribalID"  in (170258, 196840);
+  delete from cqc."WorkerAudit" where "WorkerFK" in (select distinct "ID" from cqc."Worker" where "TribalID"  in (170258, 196840));
+  delete from cqc."WorkerJobs" where "WorkerFK" in (select distinct "ID" from cqc."Worker" where "TribalID"  in (170258, 196840));
+  delete from cqc."Worker" where "TribalID"  in (170258, 196840);
+  delete from cqc."EstablishmentCapacity" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  delete from cqc."EstablishmentJobs" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  delete from cqc."EstablishmentServices" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  delete from cqc."EstablishmentLocalAuthority" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  delete from cqc."EstablishmentServiceUsers" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+
+  delete from cqc."Login" where "RegistrationID" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  delete from cqc."UserAudit" where "UserFK" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  delete from cqc."PasswdResetTracking" where "UserFK" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  delete from cqc."User" where "TribalID"  in (170258, 196840);
+  delete from cqc."EstablishmentAudit" where "EstablishmentFK" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  delete from cqc."Establishment" where "TribalID"  in (170258, 196840);
 
 
-update cqc."Establishment" set "IsRegulated"=True,"LocationID"='1-5400068426' where "EstablishmentID"=337 ;
+--- Deletion Verification 
 
---Verify the change reflected in table
+-- Selection based on Tribal ID [196840,170258]
 
-select * from cqc."Establishment" where "EstablishmentID"=337 ;
+
+select "TribalID"   from cqc."Establishment" where "NmdsID" IN ('E302906','G260465');
+
+SELECT * FROM cqc."Worker" where "TribalID" in (select "TribalID" from cqc."Establishment" where "NmdsID" IN ('E302906','G260465')) ;
+
+select  *    from cqc."WorkerQualification" where "TribalID" in (170258, 196840);
+  select  *    from cqc."WorkerTraining" where "TribalID"  in (170258, 196840);
+  select  *    from cqc."WorkerAudit" where "WorkerFK" in (select distinct "ID" from cqc."Worker" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."WorkerJobs" where "WorkerFK" in (select distinct "ID" from cqc."Worker" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."Worker" where "TribalID"  in (170258, 196840);
+  select  *    from cqc."EstablishmentCapacity" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentJobs" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentServices" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentLocalAuthority" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."EstablishmentServiceUsers" where "EstablishmentID" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+
+  select  *    from cqc."Login" where "RegistrationID" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."UserAudit" where "UserFK" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."PasswdResetTracking" where "UserFK" in (select distinct "RegistrationID" from cqc."User" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."User" where "TribalID"  in (170258, 196840);
+  select  *    from cqc."EstablishmentAudit" where "EstablishmentFK" in (select distinct "EstablishmentID" from cqc."Establishment" where "TribalID"  in (170258, 196840));
+  select  *    from cqc."Establishment" where "TribalID"  in (170258, 196840);
 
 
