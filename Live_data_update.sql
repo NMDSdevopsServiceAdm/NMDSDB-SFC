740d2c24f4da84c1614d9883b0a4632dc0690eeb
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-05-16 14:34:09 +0100

    Old and new updated

--- Live_data_update.sql
+++ Live_data_update.sql
@@ -1,3 +1,28 @@
+
+
+--https://trello.com/c/CKuDXAbq
+update cqc."Establishment" set "MainServiceFKValue"=20 where "EstablishmentID"=340 and "LocationID"='1-5254278626';
+
+--Verify the change reflected in table
+
+\x
+
+select * from cqc."Establishment" where  "EstablishmentID"=340 and "LocationID"='1-5254278626';
+
+
+
+--https://trello.com/c/oeYhp3Ui
+
+
+update cqc."Establishment" set "IsRegulated"=True,"LocationID"='1-5400068426' where "EstablishmentID"=337 ;
+
+--Verify the change reflected in table
+
+select * from cqc."Establishment" where "EstablishmentID"=337 ;
+
+
+-- Date 16 May 2019
+
 -- https://trello.com/c/Ypwr3hZ4/55-remove-parent-cohort-2-users-from-asc-wds
 
 --Deletion data view before we delete the Establishment and related records
