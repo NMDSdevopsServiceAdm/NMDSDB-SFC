483c598ead4c26125e4cbecefd567e0ff080fafc
andrewkeig <andrew.keig@gmail.com>
2019-06-21 11:41:22 +0100

    update ddl script

--- UAT/V1.10.6_patch.sql
+++ UAT/V1.10.6_patch.sql
@@ -3,9 +3,9 @@ UPDATE
 SET
 	"Type"='Utilisation'
 WHERE
-	"ServiceID" in (18, 22, 35);
+	"ServiceID" in (18, 21, 22, 35);
 
 
 INSERT INTO cqc."ServicesCapacity"
 ("ServiceCapacityID", "ServiceID", "Question", "Sequence", "Type")
-VALUES(17, 19, 'Total places Number of people using the service on the completion date', 1, 'Utilisation');
+VALUES(19, 19, 'Total places Number of people using the service on the completion date', 1, 'Utilisation');
--- create-db-ddl.sql
+++ create-db-ddl.sql
@@ -975,13 +975,13 @@ INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence"
 INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (10, 10, 1, 'How many places do you currently have?', 'Capacity');
 INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (11, 10, 2, 'Number of people using the service on the completion date', 'Utilisation');
 INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (12, 20, 1, 'Number of people using the service on the completion date', 'Utilisation');
-INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (13, 22, 1, 'Number of people receiving care on the completion date', 'Capacity');
-INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (14, 35, 1, 'Number of people receiving care on the completion date', 'Capacity');
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (13, 22, 1, 'Number of people receiving care on the completion date', 'Utilisation');
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (14, 35, 1, 'Number of people receiving care on the completion date', 'Utilisation');
 INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (15, 11, 1, 'Number of people receiving care on the completion date', 'Utilisation');
-INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (16, 18, 1, 'Number of people receiving care on the completion date', 'Capacity');
-INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (17, 21, 1, 'Number of people receiving care on the completion date', 'Capacity');
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (16, 18, 1, 'Number of people receiving care on the completion date', 'Utilisation');
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (17, 21, 1, 'Number of people receiving care on the completion date', 'Utilisation');
 INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Sequence", "Question", "Type") values (18, 23, 1, 'Number of people using the service on the completion date', 'Utilisation');
-
+INSERT INTO cqc."ServicesCapacity" ("ServiceCapacityID", "ServiceID", "Question", "Sequence", "Type") values (19, 19, 'Total places Number of people using the service on the completion date', 1, 'Utilisation');
 
 ----Jobs
 
