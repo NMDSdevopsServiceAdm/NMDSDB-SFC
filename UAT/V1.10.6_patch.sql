b4333aec1cce40a10a1bd62eaa0e71c45f50926c
andrewkeig <andrew.keig@gmail.com>
2019-06-21 09:57:42 +0100

    patch 6 updates shared lives

--- UAT/V1.10.6_patch.sql
+++ UAT/V1.10.6_patch.sql
@@ -4,3 +4,8 @@ SET
 	"Type"='Utilisation'
 WHERE
 	"ServiceID" in (11, 13, 18, 20, 22, 35);
+
+
+INSERT INTO cqc."ServicesCapacity"
+("ServiceCapacityID", "ServiceID", "Question", "Sequence", "Type")
+VALUES(17, 19, 'Total places Number of people using the service on the completion date', 1, 'Capacity');
