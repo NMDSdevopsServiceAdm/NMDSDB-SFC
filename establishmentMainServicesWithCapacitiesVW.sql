e782baae31d9effa31c8f58f5f02208fcfe10f3c
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-06-23 06:25:33 +0100

    With ServiceCapacity.Type now available on reference data, the analysis of capacities/y/utilsations can be done on explicit type rather than presumed 'Sequence'

--- establishmentMainServicesWithCapacitiesVW.sql
+++ establishmentMainServicesWithCapacitiesVW.sql
@@ -3,18 +3,19 @@ DROP VIEW IF EXISTS cqc."EstablishmentMainServicesWithCapacitiesVW";
 CREATE OR REPLACE VIEW cqc."EstablishmentMainServicesWithCapacitiesVW" AS
 SELECT
 	"EstablishmentID",
-	CASE WHEN "CAPACITY1" is null THEN -1 ELSE "CAPACITY1" END,
-	CASE WHEN "CAPACITY2" is null THEN -1 ELSE "CAPACITY2" END
+	CASE WHEN "CAPACITY" is null THEN -1 ELSE "CAPACITY" END,
+	CASE WHEN "UTILISATION" is null THEN -1 ELSE "UTILISATION" END
 FROM (
 	SELECT
 		"EstablishmentID",
-		SUM("Answer") FILTER(WHERE "Sequence"=1) "CAPACITY1",
-		SUM("Answer") FILTER(WHERE "Sequence"=2) "CAPACITY2"
+		SUM("Answer") FILTER(WHERE "Type"='Capacity') "CAPACITY",
+		SUM("Answer") FILTER(WHERE "Type"='Utilisation') "UTILISATION"
 	FROM (
 		SELECT
 			"AllEstablishmentCapacityQuestions"."EstablishmentID",
 			"AllEstablishmentCapacityQuestions"."ServiceCapacityID",
 			"AllEstablishmentCapacityQuestions"."Sequence",
+			"AllEstablishmentCapacityQuestions"."Type",
 			"AllEstablishmentCapacityQuestions"."Question",
 			"AllEstablishmentCapacities"."Answer"
 		FROM
@@ -23,13 +24,14 @@ FROM (
 					"Establishment"."EstablishmentID",
 					"ServicesCapacity"."ServiceCapacityID",
 					"ServicesCapacity"."Sequence",
+					"ServicesCapacity"."Type",
 					"ServicesCapacity"."Question"
 				FROM cqc."ServicesCapacity", cqc."Establishment"
 					INNER JOIN cqc.services on "Establishment"."MainServiceFKValue" = services.id
 					LEFT JOIN cqc."EstablishmentCapacity" on "Establishment"."EstablishmentID" = "EstablishmentCapacity"."EstablishmentID"
 				WHERE "ServicesCapacity"."ServiceID" = services.id
           		  AND services.id in (SELECT DISTINCT "ServiceID" FROM cqc."ServicesCapacity" GROUP BY "ServiceID" HAVING COUNT(0) > 1)
-				GROUP BY "Establishment"."EstablishmentID", "ServicesCapacity"."ServiceCapacityID", "ServicesCapacity"."Sequence"
+				GROUP BY "Establishment"."EstablishmentID", "ServicesCapacity"."ServiceCapacityID", "ServicesCapacity"."Type", "ServicesCapacity"."Sequence"
 				ORDER BY "Establishment"."EstablishmentID", "ServicesCapacity"."Sequence"
 			) "AllEstablishmentCapacityQuestions"
 				LEFT JOIN (
@@ -44,17 +46,18 @@ FROM (
 UNION
 SELECT
 	"EstablishmentID",
-	CASE WHEN "CAPACITY1" is null THEN -1 ELSE "CAPACITY1" END,
-	null "CAPACITY2"
+	CASE WHEN "CAPACITY" is null THEN -1 ELSE "CAPACITY" END,
+	null "UTILISATION"
 FROM (
 	SELECT
 		"EstablishmentID",
-		SUM("Answer") FILTER(WHERE "Sequence"=1) "CAPACITY1"
+		SUM("Answer") FILTER(WHERE "Type"='Capacity') "CAPACITY"
 	FROM (
 		SELECT
 			"AllEstablishmentCapacityQuestions"."EstablishmentID",
 			"AllEstablishmentCapacityQuestions"."ServiceCapacityID",
 			"AllEstablishmentCapacityQuestions"."Sequence",
+			"AllEstablishmentCapacityQuestions"."Type",
 			"AllEstablishmentCapacityQuestions"."Question",
 			"AllEstablishmentCapacities"."Answer"
 		FROM
@@ -62,14 +65,18 @@ FROM (
 				SELECT
 					"Establishment"."EstablishmentID",
 					"ServicesCapacity"."ServiceCapacityID",
+					"services"."id",
+					"services"."name",
 					"ServicesCapacity"."Sequence",
+					"ServicesCapacity"."Type",
 					"ServicesCapacity"."Question"
 				FROM cqc."ServicesCapacity", cqc."Establishment"
 					INNER JOIN cqc.services on "Establishment"."MainServiceFKValue" = services.id
 					LEFT JOIN cqc."EstablishmentCapacity" on "Establishment"."EstablishmentID" = "EstablishmentCapacity"."EstablishmentID"
 				WHERE "ServicesCapacity"."ServiceID" = services.id
           		  AND services.id in (SELECT DISTINCT "ServiceID" FROM cqc."ServicesCapacity" GROUP BY "ServiceID" HAVING COUNT(0) = 1)
-				GROUP BY "Establishment"."EstablishmentID", "ServicesCapacity"."ServiceCapacityID", "ServicesCapacity"."Sequence"
+				  AND "ServicesCapacity"."Type" = 'Capacity'
+				GROUP BY "Establishment"."EstablishmentID", "ServicesCapacity"."ServiceCapacityID", "services"."id", "services"."name", "ServicesCapacity"."Sequence"
 				ORDER BY "Establishment"."EstablishmentID", "ServicesCapacity"."Sequence"
 			) "AllEstablishmentCapacityQuestions"
 				LEFT JOIN (
@@ -80,4 +87,49 @@ FROM (
 					ON "AllEstablishmentCapacities"."ServiceCapacityID" = "AllEstablishmentCapacityQuestions"."ServiceCapacityID" AND
 					   "AllEstablishmentCapacities"."EstablishmentID" = "AllEstablishmentCapacityQuestions"."EstablishmentID"
 		ORDER BY "AllEstablishmentCapacityQuestions"."EstablishmentID", "AllEstablishmentCapacityQuestions"."Sequence") AS "EstablishmentMainServicesWithCapacities"
-	GROUP BY "EstablishmentID") AS "EstablishmentMainServicesWithCapacitiesTwo";
+	GROUP BY "EstablishmentID") AS "EstablishmentMainServicesWithCapacitiesTwo"
+UNION
+SELECT
+	"EstablishmentID",
+	null "CAPACITY",
+	CASE WHEN "UTILISATION" is null THEN -1 ELSE "UTILISATION" END
+FROM (
+	SELECT
+		"EstablishmentID",
+		SUM("Answer") FILTER(WHERE "Type"='Utilisation') "UTILISATION"
+	FROM (
+		SELECT
+			"AllEstablishmentCapacityQuestions"."EstablishmentID",
+			"AllEstablishmentCapacityQuestions"."ServiceCapacityID",
+			"AllEstablishmentCapacityQuestions"."Sequence",
+			"AllEstablishmentCapacityQuestions"."Type",
+			"AllEstablishmentCapacityQuestions"."Question",
+			"AllEstablishmentCapacities"."Answer"
+		FROM
+			(
+				SELECT
+					"Establishment"."EstablishmentID",
+					"ServicesCapacity"."ServiceCapacityID",
+					"services"."id",
+					"services"."name",
+					"ServicesCapacity"."Sequence",
+					"ServicesCapacity"."Type",
+					"ServicesCapacity"."Question"
+				FROM cqc."ServicesCapacity", cqc."Establishment"
+					INNER JOIN cqc.services on "Establishment"."MainServiceFKValue" = services.id
+					LEFT JOIN cqc."EstablishmentCapacity" on "Establishment"."EstablishmentID" = "EstablishmentCapacity"."EstablishmentID"
+				WHERE "ServicesCapacity"."ServiceID" = services.id
+          		  AND services.id in (SELECT DISTINCT "ServiceID" FROM cqc."ServicesCapacity" GROUP BY "ServiceID" HAVING COUNT(0) = 1)
+				  AND "ServicesCapacity"."Type" = 'Utilisation'
+				GROUP BY "Establishment"."EstablishmentID", "ServicesCapacity"."ServiceCapacityID", "services"."id", "services"."name", "ServicesCapacity"."Sequence"
+				ORDER BY "Establishment"."EstablishmentID", "ServicesCapacity"."Sequence"
+			) "AllEstablishmentCapacityQuestions"
+				LEFT JOIN (
+					SELECT "EstablishmentID", "EstablishmentCapacity"."ServiceCapacityID", "Answer"
+					FROM cqc."EstablishmentCapacity"
+						INNER JOIN cqc."ServicesCapacity" on "ServicesCapacity"."ServiceCapacityID" = "EstablishmentCapacity"."ServiceCapacityID"
+				) "AllEstablishmentCapacities"
+					ON "AllEstablishmentCapacities"."ServiceCapacityID" = "AllEstablishmentCapacityQuestions"."ServiceCapacityID" AND
+					   "AllEstablishmentCapacities"."EstablishmentID" = "AllEstablishmentCapacityQuestions"."EstablishmentID"
+		ORDER BY "AllEstablishmentCapacityQuestions"."EstablishmentID", "AllEstablishmentCapacityQuestions"."Sequence") AS "EstablishmentMainServicesWithCapacities"
+	GROUP BY "EstablishmentID") AS "EstablishmentMainServicesWithCapacitiesTwo"
