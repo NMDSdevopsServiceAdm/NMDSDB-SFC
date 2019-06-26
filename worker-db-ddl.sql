49538d480c27dfaf83458b0cc64689b1fa7efc5b
Darren Beck <10288173+dbjsdev@users.noreply.github.com>
2019-06-26 15:56:16 +0100

    fixes

--- worker-db-ddl.sql
+++ worker-db-ddl.sql
@@ -108,13 +108,12 @@ CREATE TYPE cqc."WorkerOtherQualifications" AS ENUM (
 );
 
 CREATE TYPE cqc."worker_registerednurse_enum" AS ENUM (
-    'Adult nurse',
-    'Mental health nurse',
-    'Learning disabiliies',
-    'Children''s nurse',
-    'Enrolled nurse'
+    'Adult Nurse',
+    'Mental Health Nurse',
+    'Learning Disabilities Nurse',
+    'Children''s Nurse',
+    'Enrolled Nurse'
 );
-
 
 
 -- Ethnicity Reference Data
@@ -933,11 +932,11 @@ CREATE TABLE IF NOT EXISTS cqc."NurseSpecialism" (
   "Other" BOOLEAN DEFAULT FALSE
 );
 
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (1, 1, 'Older people (including dementia, elderly care and end of life care)', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (1, 1, 'Older people (Including dementia, elderly care and end of life care)', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (2, 2, 'Adults', false);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (3, 3, 'Learning disability', false);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (4, 4, 'Mental health', false);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (5, 5, 'Community care', false);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (6, 6, 'Other', true);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (7, 7, 'Not applicable', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (3, 3, 'Learning Disability', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (4, 4, 'Mental Health', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (5, 5, 'Community Care', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (6, 6, 'Others', true);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (7, 7, 'Not Applicable', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (8, 8, 'Don''t know', false);
