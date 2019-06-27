cefa7c5cdf5c567d1bbb3dec7f2d26dff7569ea1
Darren Beck <10288173+dbjsdev@users.noreply.github.com>
2019-06-27 09:20:29 +0100

    spelling changes

--- UAT/V1.10.8_patch.sql
+++ UAT/V1.10.8_patch.sql
@@ -29,11 +29,11 @@ ALTER TABLE cqc."Worker"   ADD COLUMN "NurseSpecialismFKChangedAt" TIMESTAMP NUL
 ALTER TABLE cqc."Worker"   ADD COLUMN "NurseSpecialismFKSavedBy" VARCHAR(120) NULL;
 ALTER TABLE cqc."Worker"   ADD COLUMN "NurseSpecialismFKChangedBy" VARCHAR(120) NULL;
 
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (1, 1, 'Older people (Including dementia, elderly care and end of life care)', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (1, 1, 'Older people (including dementia, elderly care and end of life care)', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (2, 2, 'Adults', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (3, 3, 'Learning Disability', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (4, 4, 'Mental Health', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (5, 5, 'Community Care', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (6, 6, 'Others', true);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (7, 7, 'Not Applicable', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (7, 7, 'Not applicable', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (8, 8, 'Don''t know', false);
--- worker-db-ddl.sql
+++ worker-db-ddl.sql
@@ -115,7 +115,6 @@ CREATE TYPE cqc."worker_registerednurse_enum" AS ENUM (
     'Enrolled Nurse'
 );
 
-
 -- Ethnicity Reference Data
 CREATE TABLE IF NOT EXISTS cqc."Ethnicity" (
 	"ID" INTEGER NOT NULL PRIMARY KEY,
@@ -932,11 +931,11 @@ CREATE TABLE IF NOT EXISTS cqc."NurseSpecialism" (
   "Other" BOOLEAN DEFAULT FALSE
 );
 
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (1, 1, 'Older people (Including dementia, elderly care and end of life care)', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (1, 1, 'Older people (including dementia, elderly care and end of life care)', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (2, 2, 'Adults', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (3, 3, 'Learning Disability', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (4, 4, 'Mental Health', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (5, 5, 'Community Care', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (6, 6, 'Others', true);
-insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (7, 7, 'Not Applicable', false);
+insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (7, 7, 'Not applicable', false);
 insert into cqc."NurseSpecialism" ("ID", "Seq", "Specialism", "Other") values (8, 8, 'Don''t know', false);
