f93c9d194fde8a95a24003c8f5829b872b1820d1
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-07-01 19:06:35 +0100

    Revised patch SQL to match "LocalIdentifier" a property change managed property.

--- UAT/V1.10.9_patch.sql
+++ UAT/V1.10.9_patch.sql
@@ -1,6 +1,18 @@
 -- https://trello.com/c/AscBN35F/47-18-bulk-upload-local-identifiers-establishment
 
-ALTER TABLE cqc."Establishment" ADD COLUMN "LocalIdentifier" TEXT NULL;
+-- undo patch - run manually to fix sfcdevdb only
+--ALTER TABLE cqc."Establishment" DROP COLUMN "LocalIdentifier";
+--ALTER TABLE ONLY cqc."Establishment"
+    DROP CONSTRAINT "establishment_LocalIdentifier_unq";
+
+
+-- new patch
+ALTER TABLE cqc."Establishment" ADD COLUMN "LocalIdentifierValue" TEXT NULL;
+ALTER TABLE cqc."Establishment" ADD COLUMN "LocalIdentifierSavedAt" TEXT NULL;
+ALTER TABLE cqc."Establishment" ADD COLUMN "LocalIdentifierSavedBy" TEXT NULL;
+ALTER TABLE cqc."Establishment" ADD COLUMN "LocalIdentifierChangedAt" TEXT NULL;
+ALTER TABLE cqc."Establishment" ADD COLUMN "LocalIdentifierChangedBy" TEXT NULL;
+
 
 ALTER TABLE ONLY cqc."Establishment"
-    ADD CONSTRAINT "establishment_LocalIdentifier_unq" UNIQUE ("LocalIdentifier");
+    ADD CONSTRAINT "establishment_LocalIdentifier_unq" UNIQUE ("LocalIdentifierValue");
