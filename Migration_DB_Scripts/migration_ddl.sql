29aaf50f0cde05ad595bc48ca3e5f766740f22be
Shakir-Sopra <45394582+Shakir-Sopra@users.noreply.github.com>
2019-07-29 15:37:38 +0100

    Last Patch Applied

--- Migration_DB_Scripts/migration_ddl.sql
+++ Migration_DB_Scripts/migration_ddl.sql
@@ -253,9 +253,41 @@ BEGIN
   PERFORM migration.worker_bulk_qualifications();
 END;
 $$ LANGUAGE plpgsql;
+--Adding  a new function to call migrateUsers;
+
+ -- FUNCTION: migration.loop_estbid_users(integer)
+
+ -- DROP FUNCTION migration.loop_estbid_users(integer);
+
+CREATE OR REPLACE FUNCTION migration.loop_estbid_users(n integer DEFAULT 100)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE 
+AS $BODY$
+DECLARE
+Allestbid  REFCURSOR;
+currentestbid record;
+Begin 
+OPEN Allestbid  for 
+select establishment_id from establishment_user where establishment_id not in  (156182, 59, 248, 669, 187078, 215842, 162286, 2533, 2952, 200560, 225586, 3278, 60682, 5228, 12937, 232842, 10121, 10757, 216264, 12041, 17047, 177958, 136485,15000, 20876, 233642, 17661, 168369, 40762, 205162, 154806, 42683, 45882, 196119, 85603, 181062, 218926,196840, 144133, 215263, 170258, 217893, 231842) limit n;
+Loop
+Begin
+FETCH Allestbid INTO currentestbid;
+EXIT WHEN NOT FOUND;
+PERFORM  migration.MigrateUsers(currentestbid.establishment_id);            
+--  RAISE NOTICE ' %', current_establishment_id.id;
+END;
+END LOOP;
+END;
+$BODY$;
+
+ALTER FUNCTION migration.loop_estbid_users(integer)
+OWNER TO postgres;
+
 
 DROP FUNCTION IF EXISTS migration.MigrateUsers;
-CREATE OR REPLACE FUNCTION migration.MigrateUsers()
+CREATE OR REPLACE FUNCTION migration.MigrateUsers(estb_id int)
   RETURNS void AS $$
 DECLARE
   AllUsers REFCURSOR;
@@ -288,8 +320,7 @@ BEGIN
             on "Establishment"."TribalID" = establishment_user.establishment_id
 		    left join cqc."User" on "User"."TribalID" = users.id
     where users.status <> 4
-      and establishment_user.establishment_id in (156182, 59, 248, 669, 187078, 215842, 162286, 2533, 2952, 200560, 225586, 3278, 60682, 5228, 12937, 232842, 10121, 10757, 216264, 12041, 17047, 177958, 136485, 15000, 20876, 233642, 17661, 168369, 40762, 205162, 154806, 42683, 45882, 196119, 85603, 181062, 218926, 196840, 144133, 215263, 170258, 217893, 231842);
-
+      and establishment_user.establishment_id=estb_id ; --Removed the hardcoded values for establishmetid field passing this value from a parent function called migration.loop_estbid_users()
   LOOP
     FETCH AllUsers INTO CurrentUser;
     EXIT WHEN NOT FOUND;
