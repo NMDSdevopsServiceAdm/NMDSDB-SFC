46ff3cf27a86ccac546e1683a181e8f3679e0a37
Darren Beck <10288173+dbjsdev@users.noreply.github.com>
2019-08-12 15:52:55 +0100

    fix

--- UAT/V1.10.18_patch.sql
+++ UAT/V1.10.18_patch.sql
@@ -7,8 +7,15 @@ CREATE TYPE cqc.establishment_data_access_permission AS ENUM (
     'None'
 );
 
-ALTER TABLE cqc."Establishment" ALTER COLUMN "DataPermissions" TYPE cqc.establishment_data_access_permission USING "DataPermissions"::cqc.establishment_data_access_permission;
+ALTER TABLE cqc."Establishment" ALTER COLUMN "DataPermissions" TYPE cqc.establishment_data_access_permission USING "DataPermissions"::text::cqc.establishment_data_access_permission;
 
 ALTER TABLE cqc."Establishment" ALTER COLUMN "DataPermissions" SET DEFAULT 'None';
 
 UPDATE cqc."Establishment" SET "DataPermissions" = 'None' WHERE "DataPermissions" IS NULL;
+
+
+
+
+
+
+
