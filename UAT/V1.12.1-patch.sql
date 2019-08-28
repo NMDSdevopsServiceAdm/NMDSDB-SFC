96a0f9fc6fabf982a7541f2317ce755bafd27639
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-08-28 15:07:08 +0100

    The user's report cqc schema functions.

---
+++ UAT/V1.12.1-patch.sql
@@ -0,0 +1,84 @@
+-- https://trello.com/c/UsiGWFJU
+
+DROP FUNCTION IF EXISTS cqc.localAuthorityReportEstablishment;
+CREATE OR REPLACE FUNCTION cqc.localAuthorityReportEstablishment(establishmentID INTEGER, reportFrom DATE, reportTo DATE)
+ RETURNS BOOLEAN 
+AS $$
+DECLARE
+	success BOOLEAN;
+	v_error_msg TEXT;
+	v_error_stack TEXT;
+BEGIN
+	success := true;
+	
+	RAISE NOTICE 'localAuthorityReportEstablishment (%) from % to %', establishmentID, reportFrom, reportTo;
+
+	RETURN success;
+	
+	EXCEPTION WHEN OTHERS THEN
+		GET STACKED DIAGNOSTICS v_error_stack=PG_EXCEPTION_CONTEXT, v_error_msg=MESSAGE_TEXT;
+		RAISE WARNING 'localAuthorityReportWorker: %: %', v_error_msg, v_error_stack;
+		RETURN false;
+
+END; $$
+LANGUAGE 'plpgsql';
+
+DROP FUNCTION IF EXISTS cqc.localAuthorityReportWorker;
+CREATE OR REPLACE FUNCTION cqc.localAuthorityReportWorker(establishmentID INTEGER, reportFrom DATE, reportTo DATE)
+ RETURNS BOOLEAN 
+AS $$
+DECLARE
+	success BOOLEAN;
+	v_error_msg TEXT;
+	v_error_stack TEXT;
+BEGIN
+	success := true;
+	
+	RAISE NOTICE 'localAuthorityReportWorker (%) from % to %', establishmentID, reportFrom, reportTo;
+	
+	RETURN success;
+	
+	EXCEPTION WHEN OTHERS THEN
+		GET STACKED DIAGNOSTICS v_error_stack=PG_EXCEPTION_CONTEXT, v_error_msg=MESSAGE_TEXT;
+		RAISE WARNING 'localAuthorityReportWorker: %: %', v_error_msg, v_error_stack;
+		RETURN false;
+
+END; $$
+LANGUAGE 'plpgsql';
+
+
+DROP FUNCTION IF EXISTS cqc.localAuthorityReport;
+CREATE OR REPLACE FUNCTION cqc.localAuthorityReport(establishmentID INTEGER, reportFrom DATE, reportTo DATE)
+ RETURNS BOOLEAN 
+AS $$
+DECLARE
+	success BOOLEAN;
+	v_error_msg TEXT;
+	v_error_stack TEXT;
+	establishmentReportStatus BOOLEAN;
+	workerReportStatus BOOLEAN;
+BEGIN
+	success := true;
+	
+	RAISE NOTICE 'localAuthorityReport (%) from % to %', establishmentID, reportFrom, reportTo;
+
+	SELECT cqc.localAuthorityReportEstablishment(establishmentID, reportFrom, reportTo) INTO establishmentReportStatus;
+	SELECT cqc.localAuthorityReportWorker(establishmentID, reportFrom, reportTo) INTO workerReportStatus;
+	
+	IF NOT (establishmentReportStatus AND workerReportStatus) THEN
+		success := false;
+	END IF;
+	
+	RETURN success;
+	
+	EXCEPTION WHEN OTHERS THEN
+		GET STACKED DIAGNOSTICS v_error_stack=PG_EXCEPTION_CONTEXT, v_error_msg=MESSAGE_TEXT;
+		RAISE WARNING 'localAuthorityReport: %: %', v_error_msg, v_error_stack;
+		RETURN false;
+
+END; $$
+LANGUAGE 'plpgsql';
+
+
+
+--select cqc.localAuthorityReport(1::INTEGER, '2019-06-10'::DATE, '2019-08-10'::DATE);
