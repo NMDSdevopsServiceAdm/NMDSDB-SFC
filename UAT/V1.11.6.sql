cd3e3bd5f8bea988663d4dfc4b6d4e3197dca989
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-08-28 07:22:57 +0100

    Adding a filter to only update those establishments/workers where the LastWdfEligibility is null (not yet set).

--- UAT/V1.11.6.sql
+++ UAT/V1.11.6.sql
@@ -5,7 +5,8 @@ update
 set
 	"LastWdfEligibility" = TheSource.achieveddate::timestamp with time zone
 from cqc."20190819_wdf" as TheSource
-where "Establishment"."TribalID" = TheSource.tribalid;
+where "Establishment"."TribalID" = TheSource.tribalid
+  and "Establishment"."LastWdfEligibility" is null;
 
 update
 	cqc."Worker"
@@ -13,4 +14,5 @@ set
 	"LastWdfEligibility" = TheSource.achieveddate::timestamp with time zone
 from cqc."Establishment"
 	inner join cqc."20190819_wdf" as TheSource on "Establishment"."TribalID" = TheSource.tribalid
-where "Establishment"."EstablishmentID" = "Worker"."EstablishmentFK";
+where "Establishment"."EstablishmentID" = "Worker"."EstablishmentFK"
+  and "Worker"."LastWdfEligibility" is null;
